f51291055b38528d034cfe4c93fe1f2fdd4b6b88
mihaidanaila11 <danailateodor@yahoo.com>
2024-10-07 01:12:17 +0300

    Changed the structure of functions

--- Graph/Graph.hpp
+++ Graph/Graph.hpp
@@ -8,10 +8,14 @@
 class Graph{
 private:
     std::vector<std::vector<int>> adjacencyMatrix;
+    std::vector<std::vector<int>> adjacencyList;
 public:
     Graph(const std::string& filePath, const bool& oriented=false);
 
     void printAdjacencyMatrix() const;
+
+private:
+    std::vector<std::vector<int>> makeAdjacencyMatrix(const std::string& filePath, const bool& oriented=false) const;
 };
 
 #endif
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Graph/Graph.hpp"
+
+int main(){
+
+    Graph graph("graph.in");
+
+    graph.printAdjacencyMatrix();
+
+    return 0;
+}
